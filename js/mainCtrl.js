91a1f4ec3cbbbc575f3371e41d3f3e9e1f7fed6c
Brayden Kimball <kymboldexb@gmail.com>
2016-11-17 10:36:34 -0700

    Mini-myfirstangular-update

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,9 @@
 angular.module("friendsList")
 .controller("mainCtrl", function($scope) {
 	$scope.name = "Brayden"; 
-	$scope.friends = ["Kevin", "Josh", "Joe", "Brian", "Stephen"]
+	$scope.friends = ["Kevin", "Josh", "Joe", "Brian", "Stephen", "Matt", "Lucas"]
+	$scope.addFriend = function() {
+		$scope.friends.push($scope.newFriend);
+		$scope.newFriend = "";
+	}
 })
